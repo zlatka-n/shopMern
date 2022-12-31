7b0f66cf19033e0833862c231cddeb793a83edc1
zlatka-n <zla.b.ngu@gmail.com>
2022-12-31 11:30:50 +0100

    add GET req in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,15 @@
+import axios from "axios";
 import { useState } from "react";
 
 function App() {
  const [count, setCount] = useState(0);
+
+ axios({
+  method: "get",
+  url: "http://localhost:4000/",
+ }).then(function (response) {
+  console.log(response);
+ });
 
  return <div>Hello World!</div>;
 }
