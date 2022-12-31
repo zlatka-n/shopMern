edbd3203491811be449eb4d447b2321a6598fbf9
zlatka-n <zla.b.ngu@gmail.com>
2022-12-31 11:29:00 +0100

    add cors package

--- server/server.js
+++ server/server.js
@@ -2,9 +2,12 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
 const db = require('./conn')
+const cors = require('cors')
 
 require('dotenv').config({ path: './config.env' })
 
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('GET request made')
