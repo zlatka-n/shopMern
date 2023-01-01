0ea58dd2e75a095034c5c4552ace9341ee9a0b6c
zlatka-n <zla.b.ngu@gmail.com>
2023-01-01 21:39:13 +0100

    GET books in home screen

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,13 +5,6 @@ import { NavBar } from "./components/navBar/NavBar";
 import { Home } from "./screens/Home";
 
 function App() {
- //  axios({
- //   method: "get",
- //   url: "http://localhost:4000/",
- //  }).then(function (response) {
- //   console.log(response);
- //  });
-
  return (
   <div>
    <NavBar />
--- client/src/screens/Home.tsx
+++ client/src/screens/Home.tsx
@@ -1,5 +1,31 @@
-import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export const Home = () => {
- return <div>Home</div>;
+ const [books, setBooks] = useState([]);
+
+ useEffect(() => {
+  axios({
+   method: "get",
+   url: "http://localhost:4000/",
+  })
+   .then(function (response) {
+    const { data } = response;
+    console.log(data);
+    setBooks(data);
+   })
+   .catch((err) => console.error(err));
+ }, []);
+
+ return (
+  <div>
+   {books.map(({ title, author, description, _id }) => (
+    <div key={_id}>
+     <p>{title}</p>
+     <p>{author}</p>
+     <p>{description}</p>
+    </div>
+   ))}
+  </div>
+ );
 };
