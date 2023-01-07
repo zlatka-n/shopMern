507cf5a803347d93a2d1cd2ed0915e07b2ae602a
zlatka-n <zla.b.ngu@gmail.com>
2023-01-07 10:10:27 +0100

    compare hashed password for /login

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const db = require('../conn')
 const bcrypt = require('bcrypt')
+let hash
 
 router.post('/signup', (req, res) => {
   const email = req.body.email
@@ -19,25 +20,47 @@ router.post('/signup', (req, res) => {
         return
       }
 
-      bcrypt.hash(password, saltRounds, function (err, hash) {
-        if (err) res.send(err)
+      if (!user)
+        bcrypt.hash(password, saltRounds, function (err, hash) {
+          if (err) res.send(err)
 
-        db
-          .getUsersCollection()
-          .insertOne({ email, password: hash }, function (err) {
+          db
+            .getUsersCollection()
+            .insertOne({ email, password: hash }, function (err) {
 
-            if (err) res.send(err)
+              if (err) res.send(err)
 
-            res.status(200).send({ 'status': '200', 'message': 'New user was created' })
-          })
-      })
+              res.send({ 'status': '200', 'message': 'New user was created' })
+            })
+        })
 
     })
 
 })
 
 router.post('/login', (req, res) => {
-  res.send(`Log in: email: ${req.body.email}, password: ${req.body.password}`)
+  const email = req.body.email
+  const password = req.body.password
+
+  //1] check if user exists in db
+  db
+    .getUsersCollection()
+    .findOne({ email }, function (err, user) {
+      if (err) res.send(err)
+
+      if (!user) res.status(404).send({ "status": "404", "message": "User does not exist in db", "detail": "Ensure that email is registered" })
+
+      if (user) {
+        bcrypt.compare(password, user.password, function (err, result) {
+          if (err) res.send(err)
+
+          if (!result) res.send('wrong password')
+
+          if (result) res.send({ 'status': '200', 'message': 'User was logged in' })
+
+        })
+      }
+    })
 })
 
 module.exports = router
