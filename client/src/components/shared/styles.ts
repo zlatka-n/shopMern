87da9b957961b3dc90cb38f05853077e42219ebf
zlatka-n <zla.b.ngu@gmail.com>
2023-01-10 16:42:19 +0100

    create styles for input container

---
+++ client/src/components/shared/styles.ts
@@ -0,0 +1,11 @@
+import { css } from "@emotion/css";
+
+export const styles = {
+ input: css`
+  display: flex;
+  flex-direction: column;
+  width: 30vh;
+  margin-inline: auto;
+  row-gap: 2em;
+ `,
+};
--- client/src/screens/Login.tsx
+++ client/src/screens/Login.tsx
@@ -1,6 +1,7 @@
+import { Box } from "@mui/material";
 import { useForm } from "react-hook-form";
 import { Input } from "../components/shared/Input";
-
+import { styles } from "../components/shared/styles";
 export const Login = () => {
  const {
   handleSubmit,
@@ -10,10 +11,18 @@ export const Login = () => {
  const onSubmit = (data: any) => console.log(data);
 
  return (
-  <form onSubmit={handleSubmit(onSubmit)}>
-   <Input name="email" control={control} />
-   <Input name="password" control={control} />
-   <input type="submit" />
-  </form>
+  <Box
+   height={"90vh"}
+   marginX={10}
+   display="flex"
+   flexDirection={"column"}
+   marginTop={2}
+  >
+   <form onSubmit={handleSubmit(onSubmit)} className={styles.inputContainer}>
+    <Input name="email" control={control} />
+    <Input name="password" control={control} />
+    <input type="submit" />
+   </form>
+  </Box>
  );
 };
