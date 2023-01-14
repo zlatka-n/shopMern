04d29619e399b73bc66e4ea77784c3f8c9bdfbfc
zlatka-n <zla.b.ngu@gmail.com>
2023-01-14 22:08:19 +0100

    logged in: set/get local storage + update redux

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,16 @@
+import { useDispatch } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 import { NavBar } from "./components/navBar/NavBar";
+import { setLoginSuccess } from "./redux/accountSlice";
 import { Home } from "./screens/Home";
 import { Login } from "./screens/Login";
 
 function App() {
+ const dispatch = useDispatch();
+
+ const isLoggedIn = localStorage.getItem("isLoggedIn");
+ if (isLoggedIn) dispatch(setLoginSuccess(true));
+
  return (
   <div>
    <NavBar />
