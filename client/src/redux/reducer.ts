a1327c5cd5843400f08455adfedbcfb263cb0083
zlatka-n <zla.b.ngu@gmail.com>
2023-01-14 18:32:57 +0100

    set up store

---
+++ client/src/redux/reducer.ts
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { accountSlice } from "./accountSlice";
+
+export const store = configureStore({
+ reducer: {
+  account: accountSlice.reducer,
+ },
+ devTools: true,
+});
