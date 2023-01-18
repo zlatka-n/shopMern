4915d54223d12f1aae40bb7a4bb4c300f938b566
zlatka-n <zla.b.ngu@gmail.com>
2023-01-18 13:33:28 +0100

    add inputs in register screen

--- client/src/screens/Register.tsx
+++ client/src/screens/Register.tsx
@@ -1,3 +1,46 @@
+import { useForm } from "react-hook-form";
+import { Box, Button, Typography } from "@mui/material";
+import { Input } from "../components/shared/Input";
+import { styles } from "../components/shared/styles";
+import { SIGN_UP } from "../shared/constants";
+
 export const Register = () => {
- return <div>Register</div>;
+ const {
+  handleSubmit,
+  formState: { errors },
+  control,
+ } = useForm<any>({
+  // resolver: yupResolver(loginSchema),
+ });
+
+ const onSubmit = handleSubmit(async (data) => {
+  console.log(data);
+  //TODO: POST: /register
+ });
+
+ return (
+  <Box
+   height={"90vh"}
+   marginX={10}
+   display="flex"
+   flexDirection={"column"}
+   marginTop={5}
+  >
+   <form onSubmit={onSubmit} className={styles.inputContainer}>
+    <Typography fontSize={25}>Register</Typography>
+    <Input name="firstName" control={control} placeholder="First name" />
+    <Input name="lastName" control={control} placeholder="Last name" />
+    <Input name="email" control={control} placeholder="Email" />
+    <Input
+     name="password"
+     control={control}
+     placeholder="Password"
+     type="password"
+    />
+    <Button onClick={onSubmit} variant="contained" sx={{ paddingBlock: "1em" }}>
+     {SIGN_UP}
+    </Button>
+   </form>
+  </Box>
+ );
 };
