a3fc076c3e101c31d9280632702ded0c75b175ee
zlatka-n <zla.b.ngu@gmail.com>
2023-01-21 12:00:10 +0100

    getMyAccount info and set first name

--- client/src/screens/MyAccount.tsx
+++ client/src/screens/MyAccount.tsx
@@ -1,3 +1,20 @@
+import { useEffect, useState } from "react";
+import { getMyAccount } from "../api/myaccount";
+
 export const MyAccount = () => {
- return <div>MyAccount</div>;
+ const [firstName, setFirstName] = useState<string>("");
+
+ useEffect(() => {
+  getMyAccount().then((res) => setFirstName(res.firstName));
+ }, []);
+
+ return (
+  <div>
+   <h1>Hello {firstName},</h1>
+   <p>
+    Welcome to your account. Here you can control orders, returns or edit
+    personal information.
+   </p>
+  </div>
+ );
 };
