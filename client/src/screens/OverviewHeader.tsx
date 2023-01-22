3977afff3b51b2a9e6acc764b5a2f459d0fc5889
zlatka-n <zla.b.ngu@gmail.com>
2023-01-22 09:30:45 +0100

    add props for OverviewHeader

--- client/src/screens/OverviewHeader.tsx
+++ client/src/screens/OverviewHeader.tsx
@@ -1,14 +1,16 @@
 import { Typography } from "@mui/material";
 import { fontSizes } from "../components/shared/styles";
 
-export const OverviewHeader = ({ name }: { name: string }) => {
+type Props = {
+ heading: string;
+ text: string;
+};
+
+export const OverviewHeader = ({ heading, text }: Props) => {
  return (
   <>
-   <Typography fontSize={fontSizes.large}>Hello {name},</Typography>
-   <Typography paddingY={2}>
-    Welcome to your account. Here you can control orders, returns or edit
-    personal information.
-   </Typography>
+   <Typography fontSize={fontSizes.large}>{heading}</Typography>
+   <Typography paddingY={2}>{text} </Typography>
   </>
  );
 };
