26a487da8ac1a33f47907f6b95d66d32968b4ad4
zlatka-n <zla.b.ngu@gmail.com>
2023-01-22 21:06:02 +0100

    call getAddresses and display data

--- client/src/api/myaccount.ts
+++ client/src/api/myaccount.ts
@@ -9,6 +9,15 @@ export const getMyAccount = async () => {
  }
 };
 
+export const getAddresses = async () => {
+ try {
+  const { data } = await axiosInstance.get("myaccount/adresses");
+  return data;
+ } catch (err) {
+  throw err;
+ }
+};
+
 // export const getMyAccount = async () => {
 //  try {
 //   const response = await fetch("http://localhost:4000/myaccount", {
--- client/src/components/myAccount/Address.tsx
+++ client/src/components/myAccount/Address.tsx
@@ -1,13 +1,39 @@
+import { Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import { getAddresses } from "../../api/myaccount";
 import { OverviewHeader } from "../../screens/OverviewHeader";
+import { fontSizes } from "../shared/styles";
 import { ADDRESSES } from "./utils";
 
 export const Address = () => {
+ const [addresses, setAddresses] = useState([]);
+ const [name, setName] = useState({});
+
+ useEffect(() => {
+  getAddresses().then((result) => {
+   setName(result.userInfo);
+   setAddresses(result.addresses);
+  });
+ }, []);
+
  return (
   <div>
    <OverviewHeader
     heading={ADDRESSES}
     text="Here you can add or edit your adresses"
    />
+   {addresses.length > 0 ? (
+    <div>
+     <Typography fontSize={fontSizes.medium} fontWeight={600} marginBottom={1}>
+      {`${name.firstName} ${name.lastName}`}
+     </Typography>
+
+     <Typography>{addresses[0].address}</Typography>
+     <Typography>{addresses[0].city}</Typography>
+     <Typography>{addresses[0].zipCode}</Typography>
+     <Typography>{addresses[0].country}</Typography>
+    </div>
+   ) : null}
   </div>
  );
 };
