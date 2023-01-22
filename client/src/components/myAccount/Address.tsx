ce0a3e3517527f6f933a983339e363c0f286692e
zlatka-n <zla.b.ngu@gmail.com>
2023-01-22 09:31:29 +0100

    add Overviewheader in addresses

--- client/src/components/myAccount/Address.tsx
+++ client/src/components/myAccount/Address.tsx
@@ -1,3 +1,13 @@
+import { OverviewHeader } from "../../screens/OverviewHeader";
+import { ADDRESSES } from "./utils";
+
 export const Address = () => {
- return <div>Address</div>;
+ return (
+  <div>
+   <OverviewHeader
+    heading={ADDRESSES}
+    text="Here you can add or edit your adresses"
+   />
+  </div>
+ );
 };
