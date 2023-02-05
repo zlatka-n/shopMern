0071fef8bf1014bb41222a5070e7421d5e870818
zlatka-n <zla.b.ngu@gmail.com>
2023-02-05 08:43:34 +0100

    store id in jwt

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -64,10 +64,10 @@ router.post('/login', (req, res) => {
           if (result) {
             //TODO: add expiration to refresh token
 
-            const accessToken = jwt.sign({ email: user.email }, secretToken, { expiresIn: '15m' })
+            const accessToken = jwt.sign({ email: user.email, id: user._id.toString() }, secretToken, { expiresIn: '15m' })
 
             //TODO: invalidate token when user logs out
-            const refreshToken = jwt.sign({ email: user.email }, secretToken)
+            const refreshToken = jwt.sign({ email: user.email, id: user._id.toString() }, secretToken)
 
             res.cookie('accessToken', accessToken, { httpOnly: true })
             res.cookie('refreshToken', refreshToken, { httpOnly: true })
@@ -105,7 +105,8 @@ router.get('/refresh', (req, res) => {
   if (!refreshToken) return res.status(401).send({ 'status': '401', 'message': 'missing a refresh token' })
 
   const decodeToken = jwt_decode(refreshToken)
-  const newAccessToken = jwt.sign({ email: decodeToken.email }, secretToken, { expiresIn: '15m' })
+
+  const newAccessToken = jwt.sign({ email: decodeToken.email, id: decodeToken._id.toString() }, secretToken, { expiresIn: '15m' })
 
   res.cookie('accessToken', newAccessToken, { httpOnly: true })
 
