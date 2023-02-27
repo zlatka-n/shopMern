1962088870bf70503a8897a1839b56dcfd837876
zlatka-n <zla.b.ngu@gmail.com>
2023-02-27 19:56:29 +0100

    install reqct query

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,12 +4,19 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/reducer";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
  <React.StrictMode>
   <BrowserRouter>
    <Provider store={store}>
-    <App />
+    <QueryClientProvider client={queryClient}>
+     <App />
+     <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
    </Provider>
   </BrowserRouter>
  </React.StrictMode>
