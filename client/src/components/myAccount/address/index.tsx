b01977d60f69201cd705d8b669c61d9d9af0c6f2
zlatka-n <zla.b.ngu@gmail.com>
2023-02-27 21:08:23 +0100

    change to useQuery for GET: /addresses

--- client/src/components/myAccount/address/index.tsx
+++ client/src/components/myAccount/address/index.tsx
@@ -1,21 +1,20 @@
 import { Box } from "@mui/material";
-import { useEffect } from "react";
 import { getAddresses } from "../../../api/myaccount";
 import { OverviewHeader } from "../../../screens/OverviewHeader";
 import { ADDRESSES } from "../utils";
 import { AllAddresses } from "./AllAddresses";
-import { useDispatch, useSelector } from "react-redux";
-import { selectAddresses, setAddresses } from "../../../redux/userInfoSlice";
+import { useQuery } from "react-query";
 
 export const Address = () => {
- const addresses = useSelector(selectAddresses);
- const dispatch = useDispatch();
+ const {
+  isLoading,
+  isError,
+  data: userData,
+ } = useQuery("addresses", getAddresses);
 
- useEffect(() => {
-  getAddresses().then((result) => {
-   dispatch(setAddresses(result.addresses));
-  });
- }, []);
+ if (isLoading) return "Loading";
+
+ if (isError) return "Error";
 
  return (
   <Box maxWidth={"75vw"}>
@@ -23,7 +22,7 @@ export const Address = () => {
     heading={ADDRESSES}
     text="Here you can add or edit your adresses"
    />
-   <AllAddresses addresses={addresses} />
+   <AllAddresses addresses={userData?.addresses ?? []} />
   </Box>
  );
 };
