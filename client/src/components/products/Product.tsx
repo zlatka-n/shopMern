f82a06c1813bc6b1ec746754335b0bd5a46b9c85
zlatka-n <zla.b.ngu@gmail.com>
2023-03-05 15:27:58 +0100

    add useQuery for GET: /details

--- client/src/components/products/Product.tsx
+++ client/src/components/products/Product.tsx
@@ -1,3 +1,39 @@
+import { Typography } from "@mui/material";
+import { useQuery } from "react-query";
+import { useParams } from "react-router-dom";
+import { getBooksDetails } from "../../api/shop";
+
 export const Product = () => {
- return <div>Product Info</div>;
+ const { id } = useParams();
+
+ const { data: product } = useQuery(
+  "productDetails",
+  () => getBooksDetails(id),
+  {
+   enabled: !!id,
+  }
+ );
+
+ const { author, price, title } = product?.basicInfo || {};
+ const { ISBN10, ISBN13, dimensions, format, language, publicationDate } =
+  product?.details || {};
+
+ return (
+  <div>
+   <div>
+    <Typography>{title}</Typography>
+    <Typography>{author}</Typography>
+    <Typography>{price}</Typography>
+   </div>
+
+   <div>
+    <Typography>{ISBN10}</Typography>
+    <Typography>{ISBN13}</Typography>
+    <Typography>{dimensions}</Typography>
+    <Typography>{format}</Typography>
+    <Typography>{language}</Typography>
+    <Typography>{publicationDate}</Typography>
+   </div>
+  </div>
+ );
 };
