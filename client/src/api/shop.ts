a5e9c6cd12feacaff3b3e16e1cba510be7bac8d7
zlatka-n <zla.b.ngu@gmail.com>
2023-03-05 15:28:14 +0100

    create axios call getBooksDetails

--- client/src/api/shop.ts
+++ client/src/api/shop.ts
@@ -1,9 +1,19 @@
 import { axiosInstance } from "./axios";
-import { Book } from "./types";
+import { Book, ProductDetails } from "./types";
 
 export const getBooks = async () => {
  try {
   const { data } = await axiosInstance.get<Book[]>("/products");
+  return data;
+ } catch (err) {
+  console.error("Error during getBooks()");
+  throw err;
+ }
+};
+
+export const getBooksDetails = async (id?: string) => {
+ try {
+  const { data } = await axiosInstance.get<ProductDetails>(`/products/${id}`);
   return data;
  } catch (err) {
   console.error("Error during getBooks()");
