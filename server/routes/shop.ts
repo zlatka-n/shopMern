28f5f15667ec7899db90cbc4b606083e01b12707
zlatka-n <zla.b.ngu@gmail.com>
2023-03-05 14:35:25 +0100

    create route with GET: /products/:id

--- server/routes/shop.ts
+++ server/routes/shop.ts
@@ -1,3 +1,4 @@
+import { ObjectId } from "bson";
 import { Request, Response } from "express";
 import { Product } from "./types";
 
@@ -16,6 +17,38 @@ router.get("/", (req: Request, res: Response) => {
      .send({ status: "404", message: "resource not found" });
 
    return res.json(product);
+  });
+});
+
+router.get("/:id", (req: Request, res: Response) => {
+ const productId = new ObjectId(req.params.id);
+
+ db
+  .getDb()
+  .collection("productsDetails")
+  .aggregate([
+   {
+    $match: { _id: productId },
+   },
+   {
+    $lookup: {
+     from: "products",
+     localField: "_id",
+     foreignField: "_id",
+     as: "basicInfo",
+    },
+   },
+   {
+    $unwind: "$basicInfo",
+   },
+  ])
+  .toArray(function (err: Error, productDetail: any) {
+   if (err)
+    return res
+     .status(404)
+     .json({ message: `Product with id ${productId} could not be found.` });
+
+   return res.json(productDetail[0]);
   });
 });
 
