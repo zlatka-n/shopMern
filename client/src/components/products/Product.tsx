107a12991067681935a941dccceaf256cd2a44d9
zlatka-n <zla.b.ngu@gmail.com>
2023-03-09 12:24:35 +0100

    display all book info

--- client/src/components/products/Product.tsx
+++ client/src/components/products/Product.tsx
@@ -1,8 +1,10 @@
 import { Typography } from "@mui/material";
+import { useMemo } from "react";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import { getBooksDetails } from "../../api/shop";
 import { Details } from "../../api/types";
+import { fontSizes } from "../shared/styles";
 
 export const Product = () => {
  const { id } = useParams();
@@ -17,20 +19,28 @@ export const Product = () => {
 
  const { author, price, title } = product?.basicInfo || {};
 
+ const showDetails = useMemo(() => {
+  return product?.details
+   ? Object.keys(product?.details).map((key) => (
+      <Typography key={product?.details[key]}>
+       {product?.details[key]}
+      </Typography>
+     ))
+   : null;
+ }, [product?.details]);
+
  return (
   <div>
    <div>
-    <Typography>{title}</Typography>
-    <Typography>{author}</Typography>
-    <Typography>{price}</Typography>
+    <Typography fontSize={fontSizes.large}>{title}</Typography>
+    <Typography
+     fontSize={fontSizes.medium}
+    >{`By (author) ${author}`}</Typography>
+    <Typography>{product?.description}</Typography>
+    <Typography fontSize={fontSizes.medium}>{price}</Typography>
    </div>
-   {product?.details
-    ? Object.keys(product?.details).map((key) => (
-       <Typography key={product?.details[key]}>
-        {product?.details[key]}
-       </Typography>
-      ))
-    : null}
+   <Typography fontSize={fontSizes.medium}>Details</Typography>
+   <div>{showDetails}</div>
   </div>
  );
 };
