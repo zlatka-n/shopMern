f304011ecbbb07b0ca6cdcb3825a9d9b2309ff1b
zlatka-n <zla.b.ngu@gmail.com>
2023-03-09 13:33:46 +0100

    create grid layout for product

--- client/src/components/products/Product.tsx
+++ client/src/components/products/Product.tsx
@@ -1,9 +1,8 @@
-import { Typography } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import { useMemo } from "react";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import { getBooksDetails } from "../../api/shop";
-import { Details } from "../../api/types";
 import { fontSizes } from "../shared/styles";
 
 export const Product = () => {
@@ -18,7 +17,6 @@ export const Product = () => {
  );
 
  const { author, price, title } = product?.basicInfo || {};
-
  const showDetails = useMemo(() => {
   return product?.details
    ? Object.keys(product?.details).map((key) => (
@@ -30,17 +28,38 @@ export const Product = () => {
  }, [product?.details]);
 
  return (
-  <div>
-   <div>
-    <Typography fontSize={fontSizes.large}>{title}</Typography>
-    <Typography
-     fontSize={fontSizes.medium}
-    >{`By (author) ${author}`}</Typography>
-    <Typography>{product?.description}</Typography>
-    <Typography fontSize={fontSizes.medium}>{price}</Typography>
-   </div>
-   <Typography fontSize={fontSizes.medium}>Details</Typography>
-   <div>{showDetails}</div>
-  </div>
+  <Grid container marginX={2}>
+   <Grid container marginTop={5} marginBottom={3}>
+    <Grid
+     item
+     xs={12}
+     md={3}
+     display="flex"
+     justifyContent="center"
+     alignItems="center"
+    >
+     <img src={product?.basicInfo.image} alt={title} />
+    </Grid>
+    <Grid item md={5} xs={12}>
+     <Typography fontSize={fontSizes.large}>{title}</Typography>
+     <Typography
+      fontSize={fontSizes.medium}
+     >{`By (author) ${author}`}</Typography>
+     <Typography>{product?.description}</Typography>
+    </Grid>
+    <Grid item md={4} xs={12}>
+     <Typography fontSize={fontSizes.large}>{price}</Typography>
+     <Typography>Price includes VAT/import taxes for EU delivery</Typography>
+     <Typography>
+      Available. Expected delivery to the Czech Republic in 12-17 business days.
+     </Typography>
+    </Grid>
+   </Grid>
+
+   <Grid item xs={12} md={3}>
+    <Typography fontSize={fontSizes.medium}>Details</Typography>
+    <div>{showDetails}</div>
+   </Grid>
+  </Grid>
  );
 };
