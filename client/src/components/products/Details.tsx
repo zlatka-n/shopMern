f45b87f4d8e8f5736a719119a6ca83fe731d1bf2
zlatka-n <zla.b.ngu@gmail.com>
2023-03-11 15:20:58 +0100

    create component for Details

---
+++ client/src/components/products/Details.tsx
@@ -0,0 +1,35 @@
+import { Typography, Grid } from "@mui/material";
+import { useMemo } from "react";
+import {
+ formatCamelCaseWord,
+ setFirsLetterUpperCase,
+} from "../../shared/utils";
+import { Details as DetailsType } from "../../api/types";
+
+type Props = {
+ details?: DetailsType;
+};
+
+export const Details = ({ details }: Props) => {
+ const showDetails = useMemo(() => {
+  return details
+   ? Object.keys(details).map((key) => {
+      const title =
+       key === "publicationDate"
+        ? formatCamelCaseWord(key)
+        : setFirsLetterUpperCase(key);
+      return (
+       <Grid item xs={6} key={details[key]}>
+        <Typography>{`${title}: ${details[key]}`}</Typography>
+       </Grid>
+      );
+     })
+   : null;
+ }, [details]);
+ return (
+  <>
+   <Typography lineHeight={2}>Details</Typography>
+   <Grid container>{showDetails}</Grid>
+  </>
+ );
+};
