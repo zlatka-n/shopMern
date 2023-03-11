15f7f464b4c79a21c9ce90144c061543a9ade0b2
zlatka-n <zla.b.ngu@gmail.com>
2023-03-11 15:21:10 +0100

    add smaller components in Product

--- client/src/components/products/Product.tsx
+++ client/src/components/products/Product.tsx
@@ -1,13 +1,10 @@
 import { Button, Grid, Typography } from "@mui/material";
-import { useMemo } from "react";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import { getBooksDetails } from "../../api/shop";
-import {
- formatCamelCaseWord,
- setFirsLetterUpperCase,
-} from "../../shared/utils";
 import { fontSizes } from "../shared/styles";
+import { Description } from "./Description";
+import { Details } from "./Details";
 
 export const Product = () => {
  const { id } = useParams();
@@ -21,22 +18,6 @@ export const Product = () => {
  );
 
  const { author, price, title } = product?.basicInfo || {};
-
- const showDetails = useMemo(() => {
-  return product?.details
-   ? Object.keys(product?.details).map((key) => {
-      const title =
-       key === "publicationDate"
-        ? formatCamelCaseWord(key)
-        : setFirsLetterUpperCase(key);
-      return (
-       <Grid item xs={6} key={product?.details[key]}>
-        <Typography>{`${title}: ${product?.details[key]}`}</Typography>
-       </Grid>
-      );
-     })
-   : null;
- }, [product?.details]);
 
  return (
   <Grid
@@ -55,12 +36,11 @@ export const Product = () => {
     <Typography lineHeight={2}>
      Price <span style={{ fontSize: fontSizes.medium }}>{price}</span>
     </Typography>
-    <Typography>{product?.description}</Typography>
+    <Description description={product?.description ?? ""} />
     <Button variant="contained" sx={{ marginY: 4 }}>
      Add to basket
     </Button>
-    <Typography lineHeight={2}>Details</Typography>
-    <Grid container>{showDetails}</Grid>
+    <Details details={product?.details} />
    </Grid>
   </Grid>
  );
