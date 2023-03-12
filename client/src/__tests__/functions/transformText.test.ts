8a0d1142894006d80843353792948020f0bfd321
zlatka-n <zla.b.ngu@gmail.com>
2023-03-12 11:47:59 +0100

    create tests for functions - text transform

---
+++ client/src/__tests__/functions/transformText.test.ts
@@ -0,0 +1,16 @@
+import {
+ formatCamelCaseWord,
+ setFirsLetterUpperCase,
+} from "../../shared/utils";
+
+test("set first letter to upper case", () => {
+ const result = setFirsLetterUpperCase("hello");
+
+ expect(result).toBe("Hello");
+});
+
+test("split camel case text with first letter uppercase", () => {
+ const result = formatCamelCaseWord("helloWorld");
+
+ expect(result).toBe("Hello world");
+});
