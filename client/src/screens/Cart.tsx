e34218ed52ce6c8161c4adac1206cc51033e4a35
zlatka-n <zla.b.ngu@gmail.com>
2023-04-14 15:54:58 +0200

    add mutation for deleteItemFromCart

--- client/src/screens/Cart.tsx
+++ client/src/screens/Cart.tsx
@@ -1,6 +1,6 @@
 import { Grid, Typography } from "@mui/material";
-import { useQuery } from "react-query";
-import { getCart } from "../api/cart";
+import { useMutation, useQuery, useQueryClient } from "react-query";
+import { deleteItemFromCart, getCart } from "../api/cart";
 import { colors, fontSizes } from "../components/shared/styles";
 import { ItemCard } from "../components/cart/ItemCard";
 import { CartSummary } from "../components/cart/CartSummary";
@@ -8,12 +8,21 @@ import { CartWarning } from "../components/cart/CartWarning";
 import { MAX_ITEM_CART_QTY } from "../shared/constants";
 
 export const Cart = () => {
- const { data } = useQuery("cart", getCart);
+ const queryClient = useQueryClient();
+
+ const { data } = useQuery("cart", getCart, {
+  staleTime: 180000, //3 mins
+  refetchOnMount: false,
+ });
+ const { mutate } = useMutation((id: string) => deleteItemFromCart(id), {
+  onSuccess: () => queryClient.invalidateQueries("cart"),
+ });
 
  const doesQtyExceed = data
   ? data?.cart.items.some((product) => product.qty > MAX_ITEM_CART_QTY)
   : false;
 
+ const onClickRemove = (id: string) => () => mutate(id);
  return (
   <Grid sx={{ backgroundColor: colors.grey }} minHeight={"100vh"}>
    <Typography
@@ -25,7 +34,7 @@ export const Cart = () => {
     Your shopping cart (Items: {data?.cart.totalQty})
    </Typography>
    {doesQtyExceed ? <CartWarning /> : null}
-   {data ? (
+   {data && data.cart.items.length > 0 ? (
     <Grid container marginX={5} gap={2}>
      <Grid
       item
@@ -37,10 +46,7 @@ export const Cart = () => {
       marginBottom={4}
      >
       {data.cart.items.map((product) => (
-       <ItemCard
-        product={product}
-        onClickRemove={() => alert(`DELETE: ${product._id}`)}
-       />
+       <ItemCard product={product} onClickRemove={onClickRemove(product._id)} />
       ))}
      </Grid>
      <CartSummary data={data} />
