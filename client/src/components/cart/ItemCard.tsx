1df3ec0526429e25b3eb1e0d057ce734f19bdf0b
zlatka-n <zla.b.ngu@gmail.com>
2023-04-15 17:19:19 +0200

    use Select for qty in ItemCard

--- client/src/components/cart/ItemCard.tsx
+++ client/src/components/cart/ItemCard.tsx
@@ -1,7 +1,11 @@
-import { Grid, IconButton, Typography } from "@mui/material";
+import { Grid, IconButton, Stack, Typography } from "@mui/material";
 import { CartItem } from "../../api/types";
 import { colors, fontSizes } from "../shared/styles";
 import DeleteIcon from "@mui/icons-material/Delete";
+import { useForm } from "react-hook-form";
+import { Select } from "../shared/Select";
+import { useEffect } from "react";
+import { itemQtySelection } from "../../shared/constants";
 
 type Props = {
  product: CartItem;
@@ -10,6 +14,11 @@ type Props = {
 
 export const ItemCard = ({ product, onClickRemove }: Props) => {
  const totalProductPrice = product.qty * product.price;
+ const { control, setValue } = useForm<any>();
+
+ useEffect(() => {
+  setValue(product._id, product.qty);
+ }, []);
 
  return (
   <Grid
@@ -25,15 +34,33 @@ export const ItemCard = ({ product, onClickRemove }: Props) => {
   >
    <img src={product.image} width={150} height={200} />
    <Grid item marginLeft={3} xs={7}>
-    <Typography fontSize={fontSizes.medium}>{product.title}</Typography>
+    <Typography fontSize={fontSizes.medium} lineHeight={2}>
+     {product.title}
+    </Typography>
     <Typography fontSize={fontSizes.small}>{product.author}</Typography>
-    <Typography fontSize={fontSizes.medium} color={colors.orange}>
+    <Typography
+     fontSize={fontSizes.medium}
+     lineHeight={2}
+     color={colors.orange}
+    >
      {product.price} EUR
     </Typography>
    </Grid>
    <Grid>
-    <Typography>Quantity: {product.qty}</Typography>
-    <Typography fontSize={fontSizes.medium} color={colors.orange}>
+    <Stack direction={"row"} alignItems="center" spacing={2}>
+     <Typography>Quantity:</Typography>
+     <Select
+      control={control}
+      name={product._id}
+      defaultValue={product.qty}
+      data={itemQtySelection}
+     />
+    </Stack>
+    <Typography
+     fontSize={fontSizes.medium}
+     color={colors.orange}
+     lineHeight={2}
+    >
      {totalProductPrice} EUR
     </Typography>
 
