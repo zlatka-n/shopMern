ff5023c9d6f31267b1089902a5d5ea8e1d935295
zlatka-n <zla.b.ngu@gmail.com>
2023-04-24 13:06:46 +0200

    add loader to btn

--- client/src/components/shared/AddToBasketBtn.tsx
+++ client/src/components/shared/AddToBasketBtn.tsx
@@ -1,17 +1,18 @@
-import { Button } from "@mui/material";
+import { Button, CircularProgress } from "@mui/material";
 
 type Props = {
  onClick: () => void;
+ isLoading: boolean;
 };
 
-export const AddToBasketBtn = ({ onClick }: Props) => {
+export const AddToBasketBtn = ({ onClick, isLoading }: Props) => {
  return (
   <Button
    variant="contained"
-   sx={{ marginY: 2, textTransform: "none" }}
+   sx={{ marginY: 2, textTransform: "none", minWidth: 120 }}
    onClick={onClick}
   >
-   Add to basket
+   {isLoading ? <CircularProgress color="info" size={25} /> : "Add to basket"}
   </Button>
  );
 };
--- client/src/screens/Home.tsx
+++ client/src/screens/Home.tsx
@@ -8,9 +8,11 @@ import { AddToBasketBtn } from "../components/shared/AddToBasketBtn";
 import { colors, fontSizes } from "../components/shared/styles";
 import { CheckoutModal } from "../components/cart/modal/CheckoutModal";
 import { useHandleModal } from "../shared/utils";
+import { useState } from "react";
 
 export const Home = () => {
  const { open, handleClose, handleOpen } = useHandleModal();
+ const [selectedBtnId, setSelectedBtnId] = useState("");
 
  const queryClient = useQueryClient();
 
@@ -19,18 +21,20 @@ export const Home = () => {
   refetchOnMount: false,
  });
 
- const { data: newCartItems, mutate } = useMutation(
-  (item: ItemId) => postAddToCart(item),
-  {
-   onSuccess: () => {
-    queryClient.invalidateQueries("cart");
-    handleOpen();
-   },
-  }
- );
+ const {
+  data: newCartItems,
+  mutate,
+  isLoading,
+ } = useMutation((item: ItemId) => postAddToCart(item), {
+  onSuccess: () => {
+   queryClient.invalidateQueries("cart");
+   handleOpen();
+  },
+ });
 
  const onClickAddToBasket = (itemId: string) => () => {
   mutate({ itemId });
+  setSelectedBtnId(itemId);
  };
 
  return (
@@ -40,38 +44,43 @@ export const Home = () => {
    </Grid>
    <Grid item xs={9}>
     <Grid container gap={4}>
-     {books?.map(({ title, author, _id, price, image }) => (
-      <div key={_id}>
-       <Link key={_id} to={`/${_id}`} style={{ textDecoration: "none" }}>
-        <img src={image} width={150} height={200} alt={title} />
-        <Typography
-         title={title}
-         maxWidth={150}
-         fontSize={fontSizes.medium}
-         overflow="hidden"
-         whiteSpace="nowrap"
-         textOverflow="ellipsis"
-         marginTop={2}
-         color={colors.black}
-        >
-         {title}
-        </Typography>
-        <Typography fontSize={fontSizes.small} color={colors.black}>
-         {author}
-        </Typography>
-        <Typography fontSize={fontSizes.medium} color={colors.orange}>
-         {price}
-        </Typography>
-       </Link>
-       <AddToBasketBtn onClick={onClickAddToBasket(_id)} />
-       <CheckoutModal
-        open={open}
-        onClose={handleClose}
-        totalPrice={newCartItems?.cart.totalPrice}
-        totalQty={newCartItems?.cart.totalQty}
-       />
-      </div>
-     ))}
+     {books?.map(({ title, author, _id, price, image }) => {
+      return (
+       <div key={_id}>
+        <Link key={_id} to={`/${_id}`} style={{ textDecoration: "none" }}>
+         <img src={image} width={150} height={200} alt={title} />
+         <Typography
+          title={title}
+          maxWidth={150}
+          fontSize={fontSizes.medium}
+          overflow="hidden"
+          whiteSpace="nowrap"
+          textOverflow="ellipsis"
+          marginTop={2}
+          color={colors.black}
+         >
+          {title}
+         </Typography>
+         <Typography fontSize={fontSizes.small} color={colors.black}>
+          {author}
+         </Typography>
+         <Typography fontSize={fontSizes.medium} color={colors.orange}>
+          {price}
+         </Typography>
+        </Link>
+        <AddToBasketBtn
+         onClick={onClickAddToBasket(_id)}
+         isLoading={selectedBtnId === _id ? isLoading : false}
+        />
+        <CheckoutModal
+         open={open}
+         onClose={handleClose}
+         totalPrice={newCartItems?.cart.totalPrice}
+         totalQty={newCartItems?.cart.totalQty}
+        />
+       </div>
+      );
+     })}
     </Grid>
    </Grid>
   </Grid>
