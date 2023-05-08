1e1893f950bdccbe474bc4d8d07664cfe84a9421
zlatka-n <zla.b.ngu@gmail.com>
2023-05-08 17:47:52 +0200

    display verification status with getVerifyAccount

--- client/src/screens/VerifyAccount.tsx
+++ client/src/screens/VerifyAccount.tsx
@@ -1,21 +1,65 @@
-import { useMutation } from "react-query";
+import { useQuery } from "react-query";
 import { useLocation } from "react-router-dom";
-import { VerifyAccount as VerifyAccountType } from "../api/types";
-import { postVerifyAccount } from "../api/auth";
-import { useEffect } from "react";
+import { getVerifyAccount } from "../api/auth";
+import { Box } from "@mui/material";
+import CircularProgress from "@mui/material/CircularProgress";
+import { NotFound } from "../components/shared/NotFound";
+import { styles } from "../components/shared/styles";
 
 export const VerifyAccount = () => {
  const query = new URLSearchParams(useLocation().search);
  const token = query.get("token");
  const email = query.get("email");
 
- const { mutate } = useMutation({
-  mutationFn: (reqBody: VerifyAccountType) => postVerifyAccount(reqBody),
- });
+ const urlParams = {
+  token,
+  email,
+ };
 
- useEffect(() => {
-  if (token && email) mutate({ token, email });
- }, []);
+ const { isLoading, isSuccess } = useQuery(
+  ["getVerifyAccountId", urlParams],
+  () => getVerifyAccount(urlParams)
+ );
 
- return <div>VerifyAccount</div>;
+ const displayVerificationStatus = () => {
+  if (!email) {
+   return (
+    <div className={styles.inputContainer}>
+     <NotFound />
+    </div>
+   );
+  }
+
+  if (isLoading) {
+   return (
+    <Box display="flex" justifyContent="center">
+     <CircularProgress />{" "}
+    </Box>
+   );
+  }
+
+  if (isSuccess) {
+   return (
+    <div className={styles.inputContainer}>Your account was verified</div>
+   );
+  }
+
+  return (
+   <div className={styles.inputContainer}>
+    Your account could not be verified
+   </div>
+  );
+ };
+
+ return (
+  <Box
+   height={"90vh"}
+   marginX={10}
+   display="flex"
+   flexDirection={"column"}
+   marginTop={5}
+  >
+   {displayVerificationStatus()}
+  </Box>
+ );
 };
