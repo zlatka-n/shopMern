04c2d789d7f8bb608709aa7d7d5c6c8c81e417d5
bng <zla.b.ngu@gmail.com>
2023-09-24 18:40:26 +0200

    add postPay, delete old checkcout calls

--- client/src/api/cart.ts
+++ client/src/api/cart.ts
@@ -1,61 +1,52 @@
-import { axiosInstance } from "./axios";
-import { Cart, CartItems, ItemId } from "./types";
+import { axiosInstance } from './axios';
+import { Cart, CartItems, ItemId } from './types';
 
 export const getCart = async () => {
- try {
-  const { data } = await axiosInstance.get<CartItems>("/cart");
+  try {
+    const { data } = await axiosInstance.get<CartItems>('/cart');
 
-  const { csrfToken } = data;
-  if (csrfToken) axiosInstance.defaults.headers["x-csrf-token"] = csrfToken;
+    const { csrfToken } = data;
+    if (csrfToken) axiosInstance.defaults.headers['x-csrf-token'] = csrfToken;
 
-  return data;
- } catch (err) {
-  throw err;
- }
+    return data;
+  } catch (err) {
+    throw err;
+  }
 };
 
 export const postAddToCart = async (reqBody: ItemId) => {
- try {
-  const { data } = await axiosInstance.post<Cart>("/cart", reqBody);
-  return data;
- } catch (err) {
-  throw err;
- }
+  try {
+    const { data } = await axiosInstance.post<Cart>('/cart', reqBody);
+    return data;
+  } catch (err) {
+    throw err;
+  }
 };
 
 export const deleteItemFromCart = async (id: string) => {
- try {
-  const { data } = await axiosInstance.delete(`/cart/${id}`);
-  return data;
- } catch (err) {
-  throw err;
- }
+  try {
+    const { data } = await axiosInstance.delete(`/cart/${id}`);
+    return data;
+  } catch (err) {
+    throw err;
+  }
 };
 
 export const putItemQuantity = async (reqBody: any) => {
- const { id, qty } = reqBody;
- try {
-  const { data } = await axiosInstance.put<any>(`/cart/${id}`, { qty });
-  return data;
- } catch (err) {
-  throw err;
- }
+  const { id, qty } = reqBody;
+  try {
+    const { data } = await axiosInstance.put<any>(`/cart/${id}`, { qty });
+    return data;
+  } catch (err) {
+    throw err;
+  }
 };
 
-export const postCheckoutSession = async () => {
- try {
-  const { data } = await axiosInstance.post<any>("/order/create-checkout");
-  return data;
- } catch (err) {
-  throw err;
- }
+export const postPay = async (email: string) => {
+  try {
+    const { data } = await axiosInstance.post('/order/pay', { email });
+    return data;
+  } catch (err) {
+    throw err;
+  }
 };
-
-export const getPaymentIntent = async () => {
- try {
-  const { data } = await axiosInstance.get("/order/payment-intent");
-  return data;
- } catch (err) {
-  throw err;
- }
-}
