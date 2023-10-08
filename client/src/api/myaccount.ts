40928a9a4062ec2c50ac71824ea9051ae7bde89e
zlatka-n <zla.b.ngu@gmail.com>
2023-10-08 10:21:38 +0200

    lint api mycaccount

--- client/src/api/myaccount.ts
+++ client/src/api/myaccount.ts
@@ -1,61 +1,61 @@
-import { axiosInstance } from "./axios";
-import { UserAddress } from "./types";
+import { axiosInstance } from './axios';
+import { UserAddress } from './types';
 
-const userId = window.sessionStorage.getItem("userId");
+const userId = window.sessionStorage.getItem('userId');
 
 export const getMyAccount = async () => {
- try {
-  const { data } = await axiosInstance.get("/myaccount");
-  return data;
- } catch (err) {
-  throw err;
- }
+  try {
+    const { data } = await axiosInstance.get('/myaccount');
+    return data;
+  } catch (err) {
+    throw err;
+  }
 };
 
 export const getAddresses = async () => {
- try {
-  const { data } = await axiosInstance.get<UserAddress>("myaccount/adresses");
-  return data;
- } catch (err) {
-  throw err;
- }
+  try {
+    const { data } = await axiosInstance.get<UserAddress>('myaccount/adresses');
+    return data;
+  } catch (err) {
+    throw err;
+  }
 };
 
 export const putAddress = async (reqBody: any) => {
- const userId = window.sessionStorage.getItem("userId");
+  const userId = window.sessionStorage.getItem('userId');
 
- try {
-  const { data } = await axiosInstance.put(
-   `myaccount/adresses/${userId}`,
-   reqBody
-  );
-  return data;
- } catch (err) {
-  throw err;
- }
+  try {
+    const { data } = await axiosInstance.put(
+      `myaccount/adresses/${userId}`,
+      reqBody,
+    );
+    return data;
+  } catch (err) {
+    throw err;
+  }
 };
 
 export const postAddress = async (reqBody: any) => {
- const userId = window.sessionStorage.getItem("userId");
+  const userId = window.sessionStorage.getItem('userId');
 
- try {
-  const { data } = await axiosInstance.post(
-   `myaccount/adresses/${userId}`,
-   reqBody
-  );
-  return data;
- } catch (err) {
-  throw err;
- }
+  try {
+    const { data } = await axiosInstance.post(
+      `myaccount/adresses/${userId}`,
+      reqBody,
+    );
+    return data;
+  } catch (err) {
+    throw err;
+  }
 };
 
 export const deleteAddress = async (reqBody: any) => {
- try {
-  const { data } = await axiosInstance.delete(`myaccount/adresses/${userId}`, {
-   data: { _id: reqBody },
-  });
-  return data;
- } catch (err) {
-  throw err;
- }
+  try {
+    const { data } = await axiosInstance.delete(`myaccount/adresses/${userId}`, {
+      data: { _id: reqBody },
+    });
+    return data;
+  } catch (err) {
+    throw err;
+  }
 };
